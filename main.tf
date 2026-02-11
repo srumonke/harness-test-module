08e8a26c0e762622561e29ce61209114b7aa6c14
srumonkatharness <sruthi.krishnamurthy@harness.io>
2026-02-11 12:53:52 +0530

    Consolidate to root main.tf and set version to 1.4.0

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-# --- PROVIDER & QUOTAS ---
 terraform {
   required_version = ">= 1.5.7"
   required_providers {
@@ -14,41 +13,22 @@ provider "harness" {
   account_id = "T_JG6UCfQcye3MFhGUx3tw"
 }
 
-# --- VARIABLES ---
 variable "org_name" {
-  type        = string
-  description = "Name of the organization"
+  type = string
 }
 
-variable "user_group_name" {
-  type        = string
-  default     = "platform-team"
-  description = "Name of the user group to create via submodule"
-}
-
-# --- RESOURCES ---
 resource "harness_platform_organization" "this" {
   identifier  = "simple_org_test"
   name        = var.org_name
-  description = "Simple organization created via unified main.tf"
+  description = "Unified organization"
   tags        = ["managed-by:terraform"]
 }
 
-# --- SUBMODULE CALL FROM REGISTRY ---
 module "rbac" {
-  # Format: <HOST>/<ACCOUNT>/<MODULE>/harness//modules/rbac
+  # Note: The version here should match the tag you are about to push
   source  = "app.harness.io/T_JG6UCfQcye3MFhGUx3tw/harness_test_module/harness//modules/rbac"
-  version = "1.1.0"
+  version = "1.4.0"
   
   org_id          = harness_platform_organization.this.id
-  user_group_name = var.user_group_name
-}
-
-# --- OUTPUTS ---
-output "org_id" {
-  value = harness_platform_organization.this.id
-}
-
-output "user_group_id" {
-  value = module.rbac.user_group_id
+  user_group_name = "platform-team"
 }
